cbe9a8f2e88e763c7f910645f2a30f1afbf6a18c
yflop0 <125532795+yflop0@users.noreply.github.com>
2023-04-20 13:18:25 +0800

    Create lessonmaterial.sql

---
+++ lessonmaterial.sql
@@ -0,0 +1,6 @@
+CREATE TABLE students (sid INTEGER primary key, /*create table in lowercase also can*/
+/* field name, data type, constraint*/
+                       name TEXT not null, /*must enter student name*/
+                       classid TEXT,
+                       phone TEXT /*impt!! adding country code like +65 the plus is text*/
+                       
