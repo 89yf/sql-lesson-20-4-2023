d69f63b48c40113e2ef6a216e3d04ba9cdbfb793
yflop0 <125532795+yflop0@users.noreply.github.com>
2023-04-20 13:25:47 +0800

    Update lessonmaterial.sql

--- lessonmaterial.sql
+++ lessonmaterial.sql
@@ -1,6 +1,9 @@
 CREATE TABLE students (sid INTEGER primary key, /*create table in lowercase also can*/
 /* field name, data type, constraint*/
                        name TEXT not null, /*must enter student name*/
-                       classid TEXT,
-                       phone TEXT /*impt!! adding country code like +65 the plus is text*/
+                       classid TEXT, /*no need constraint and CANNOT BE UNIQUE*/
+                       phone TEXT unique); /*impt!! adding country code like +65 the plus is text*/
+                       /*if u treat phone as a number, the data type comes with 
+                       mathematical operations, not helpful for phone numbers!!*/
+                       
                        
